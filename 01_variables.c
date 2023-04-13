05c00799d13673e239b228b94c0aaf862f7db741
Vinay Kumar Joshi <joshivinay085@gmail.com>
2023-04-14 00:34:58 +0530

    Add files via upload

---
+++ 01_variables.c
@@ -0,0 +1,38 @@
+ #include<stdio.h>
+ int main(){
+ 	printf("Hello, I am learning c with Vinay\n");
+ 	
+// 	This is a single line comment
+/*This is a multiple line comment
+it is used to comment multiple lines*/
+/*Types of variables:
+1.Integer variables --> int a = 3;
+2.Real variables --> float a = 8.5;
+3.Character variables --> char a = 'B'
+
+printf("This is %d", i);
+%d for integers
+%f for real values
+%c for characters*/
+	int a = 4;
+	float b = 8.5;
+	char c = 'V';
+	printf("The value of a is %d\n", a);
+	printf("The value of b is %f\n", b);
+	printf("The value of c is %c\n", c);
+	int d = 6;
+	printf("The sum of a and d is %d\n", a+d);
+	
+//	Create a program to add two numbers in C
+	int x = 5;
+	int y = 10;
+	int sum = x+y;
+	printf("The sum of 5 and 10 is %d\n", sum);
+	return 0;
+	
+	
+	
+	
+	
+
+}
